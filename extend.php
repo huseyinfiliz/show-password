4fe703b716b84188e0038a7a2ff0c9177284d435
huseyinfiliz <mysuperuser01@gmail.com>
2026-03-11 13:51:41 +0000

    chore: update to FoF standards
    - Update flarum/core requirement to ^1.8.0
    - Add phpstan, testing dev dependencies and scripts
    - Add flarum-cli modules configuration
    - Add GitHub Actions workflows (backend + frontend)
    - Add phpstan.neon, .styleci.yml, .editorconfig, .gitattributes
    - Add test infrastructure (phpunit configs, setup script)
    - Switch from npm to yarn, add yarn.lock
    - Update extend.php with proper header and PSR-12 formatting
    - Add .gitignore for vendor, node_modules, cache files
    - Rebuild JS dist with current webpack

--- extend.php
+++ extend.php
@@ -1,8 +1,18 @@
 <?php
-namespace huseyinfiliz\ShowPassword;
+
+/*
+ * This file is part of huseyinfiliz/show-password.
+ *
+ * Copyright (c) Huseyin Filiz.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Flarum\Extend;
+
 return [
     (new Extend\Frontend('forum'))
-        ->css(__DIR__ . '/less/forum.less')
-        ->js(__DIR__ . '/js/dist/forum.js'),
+        ->css(__DIR__.'/less/forum.less')
+        ->js(__DIR__.'/js/dist/forum.js'),
 ];
